4256d26b8649357728114e73ff8ff2ad9c70099a
Raff Viglianti <raffaeleviglianti@gmail.com>
2017-11-26 14:22:57 -0500

    Trying out better layout

--- src/components/Element.js
+++ src/components/Element.js
@@ -42,31 +42,43 @@ export default class Element extends Component {
     return (<div className="romajs-form">
       <h1 className="mdc-typography--headline">&lt;{this.props.element.ident}&gt;</h1>
 
-      <label htmlFor="fouc">Alternative identifier</label>
-      <div className="mdc-textfield mdc-textfield--upgraded">
-        <input id="fouc" type="text" className="mdc-textfield__input" value={this.state.altIdent} onChange={this.changeIdent}/>
-        <div className="mdc-textfield__bottom-line" style={{transformOrigin: '145px center'}}/>
+      <div className="mdc-layout-grid">
+        <div className="mdc-layout-grid__inner">
+          <div className="mdc-layout-grid__cell--span-3">
+            <label htmlFor="fouc">Alternative identifier</label>
+          </div>
+          <div className="mdc-layout-grid__cell--span-8">
+            <div className="mdc-textfield mdc-textfield--upgraded">
+              <input id="fouc" type="text" className="mdc-textfield__input" value={this.state.altIdent} onChange={this.changeIdent}/>
+              <div className="mdc-textfield__bottom-line" style={{transformOrigin: '145px center'}}/>
+            </div>
+            <p className="mdc-textfield-helptext mdc-textfield-helptext--persistent">
+              All documentation elements in ODD have a canonical name, supplied as the value for their ident attribute.
+              <br/>The altIdent element is used to supply an alternative name for the corresponding XML object, perhaps in a different language.
+            </p>
+          </div>
+        </div>
+        <div className="mdc-layout-grid__inner">
+          <div className="mdc-layout-grid__cell--span-3">
+            <label htmlFor="textarea">Description</label>
+          </div>
+          <div className="mdc-layout-grid__cell--span-8">
+            <AceEditor
+              ref="ace_desc"
+              mode="xml"
+              theme="tomorrow"
+              name="ace_desc"
+              fontSize={14}
+              showPrintMargin={false}
+              showGutter={true}
+              highlightActiveLine={true}
+              value={this.props.element.desc[0]}
+              editorProps={{
+                $blockScrolling: Infinity
+              }}/>
+          </div>
+        </div>
       </div>
-      <p className="mdc-textfield-helptext mdc-textfield-helptext--persistent">
-        All documentation elements in ODD have a canonical name, supplied as the value for their ident attribute.
-        <br/>The altIdent element is used to supply an alternative name for the corresponding XML object, perhaps in a different language.
-      </p>
-
-      <label htmlFor="textarea">Description</label>
-      <AceEditor
-        ref="ace_desc"
-        mode="xml"
-        theme="tomorrow"
-        name="ace_desc"
-        fontSize={14}
-        showPrintMargin={false}
-        showGutter={true}
-        highlightActiveLine={true}
-        value={this.props.element.desc[0]}
-        editorProps={{
-          $blockScrolling: Infinity
-        }}/>
-
       <div><Link to="/members">Back</Link></div>
     </div>)
   }
