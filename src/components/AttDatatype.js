2803a6837e233f4f77f0ee102fb7e15602cf0870
raffazizzi <raffaeleviglianti@gmail.com>
2018-08-30 16:10:06 -0400

    removed junk

--- src/components/AttDatatype.js
+++ src/components/AttDatatype.js
@@ -1,53 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-// import { MDCSelect } from '@material/select'
 import DatatypePicker from '../containers/DatatypePicker'
 
 export default class Attribute extends Component {
-  componentDidMount() {
-    // const select = new MDCSelect(this.refs.usage)
-    // let type = ''
-    // if (this.props.attribute.valList) {
-    //   type = this.props.attribute.valList.type
-    // }
-    // switch (type) {
-    //   case '':
-    //     select.foundation_.setSelectedIndex(0)
-    //     break
-    //   case 'closed':
-    //     select.foundation_.setSelectedIndex(1)
-    //     break
-    //   case 'semi':
-    //     select.foundation_.setSelectedIndex(2)
-    //     break
-    //   case 'open':
-    //     select.foundation_.setSelectedIndex(3)
-    //     break
-    //   default:
-    //     select.foundation_.setSelectedIndex(0)
-    // }
-    // select.listen('MDCSelect:change', () => {
-    //   let value = 'def'
-    //   switch (select.value) {
-    //     case 'Default (optional)':
-    //       value = ''
-    //       break
-    //     case 'Closed':
-    //       value = 'closed'
-    //       break
-    //     case 'Semi-Open':
-    //       value = 'semi'
-    //       break
-    //     case 'Open':
-    //       value = 'open'
-    //       break
-    //     default:
-    //       value = 'def'
-    //   }
-    //   this.props.setValListType(value)
-    // })
-  }
-
   render() {
     let datatype = this.props.attribute.datatype.dataRef.key
     if (!datatype) {
