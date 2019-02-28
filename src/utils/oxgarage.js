1127fd1d3726cf2248728aa05ed19549ce7ca93f
raffazizzi <raffaeleviglianti@gmail.com>
2019-02-28 16:30:13 -0500

    Moved to UMD dev oxgarage

--- src/utils/oxgarage.js
+++ src/utils/oxgarage.js
@@ -1,9 +1,9 @@
 // const location = 'www.tei-c.org'
 // const location = 'localhost:8080'
 // const location = 'oxgarage.euryanthe.de'
-const location = 'simorgh.umd.edu:7002'
+const location = 'oxgarage.mith.us'
 // const protocol = ''
-const protocol = 'http:'
+const protocol = 'https:'
 
 export default {
   compile_json: `${protocol}//${location}/ege-webservice/Conversions/ODD%3Atext%3Axml/ODDC%3Atext%3Axml/oddjson%3Aapplication%3Ajson/conversion?properties=%3Cconversions%3E%3Cconversion%20index%3D%220%22%3E%3Cproperty%20id%3D%22oxgarage.getImages%22%3Efalse%3C%2Fproperty%3E%3Cproperty%20id%3D%22oxgarage.getOnlineImages%22%3Efalse%3C%2Fproperty%3E%3Cproperty%20id%3D%22oxgarage.lang%22%3Een%3C%2Fproperty%3E%3Cproperty%20id%3D%22oxgarage.textOnly%22%3Etrue%3C%2Fproperty%3E%3Cproperty%20id%3D%22pl.psnc.dl.ege.tei.profileNames%22%3Edefault%3C%2Fproperty%3E%3C%2Fconversion%3E%3C%2Fconversions%3E`,
