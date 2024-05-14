49ea7c7f93f474edb237a8350cf1d662e23e9c6e
raffazizzi <raffaeleviglianti@gmail.com>
2024-05-14 10:07:04 -0400

    Added dialog when downloading via TEIGarage. Fixes #160

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -8,9 +8,13 @@ import InfoDialog from './dialogs/Info'
 export default class DownloadButton extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      showInfoDialog: false
+    const i18n = _i18n(this.props.language, 'DownloadButton')
+    this.initialState = {
+      showInfoDialog: false,
+      infoHeader: i18n('The customization is not valid'),
+      infoBody: i18n('errormsg')
     }
+    this.state = this.initialState
   }
 
   componentDidMount() {
@@ -20,6 +24,20 @@ export default class DownloadButton extends Component {
   render() {
     // Set language function
     const i18n = _i18n(this.props.language, 'DownloadButton')
+
+    const handleDownload = (format) => {
+      if (format === 'customization') {
+        this.props.downloadCustomization()
+      } else {
+        this.setState({
+          showInfoDialog: true,
+          infoHeader: i18n('Generating the Customization'),
+          infoBody: i18n('geninfo')
+        })
+        this.props.downloadSchema(format)
+      }
+    }
+
     return (
       <div style={{position: 'relative'}}>
         <button disabled={!this.props.isLoaded} className="mdc-button mdc-button--raised toggle" onClick={() => {
@@ -34,60 +52,60 @@ export default class DownloadButton extends Component {
         <div style={{position: 'relative'}}>
           <div key="opts" className="mdc-menu mdc-menu-surface" tabIndex="-1" ref="menu">
             <ul className="mdc-list" role="menu" aria-hidden="true">
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadCustomization()}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('customization')}}>
                 {i18n('Customization as ODD')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('compiled.odd')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('compiled.odd')}}>
                 {i18n('Compiled ODD')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('rng')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('rng')}}>
                 {i18n('RELAX NG schema')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('rnc')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('rnc')}}>
                 {i18n('RELAX NG compact')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('w3c')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('w3c')}}>
                 {i18n('W3C schema')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('dtd')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('dtd')}}>
                 {i18n('DTD')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('isosch')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('isosch')}}>
                 {i18n('ISO Schematron constraints')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('html')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('html')}}>
                 {i18n('Documentation as HTML')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('tei')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('tei')}}>
                 {i18n('Documentation as TEI Lite')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('docx')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('docx')}}>
                 {i18n('Documentation as MS Word')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('latex')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('latex')}}>
                 {i18n('Documentation as LaTeX')}
               </li>
               <li className="mdc-list-divider" role="separator"/>
-              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{this.props.downloadSchema('pdf')}}>
+              <li className="mdc-list-item" role="menuitem" tabIndex="0" onClick={()=>{handleDownload('pdf')}}>
                 {i18n('Documentation as PDF')}
               </li>
             </ul>
           </div>
         </div>
-        <InfoDialog key="id" show={this.state.showInfoDialog} hide={() => {this.setState({showInfoDialog: false})}}
-          header={i18n('The customization is not valid')} body={i18n('errormsg')} />
+        <InfoDialog key="id" show={this.state.showInfoDialog} hide={() => {this.setState(this.initialState)}}
+          header={this.state.infoHeader} body={this.state.infoBody} />
       </div>
     )
   }
