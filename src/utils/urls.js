9af8767b17e9bc9beabdf026191f05c4168d679f
Raff Viglianti <raffaeleviglianti@gmail.com>
2024-11-05 13:51:27 -0500

    restored url to teigarage

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -5,11 +5,11 @@
    Set custom URLs values here.
 */
 
-// export const TEIGARAGE_PROTOCOL = 'https'
-// export const TEIGARAGE_LOCATION = 'teigarage.tei-c.org'
+export const TEIGARAGE_PROTOCOL = 'https'
+export const TEIGARAGE_LOCATION = 'teigarage.tei-c.org'
 
-export const TEIGARAGE_PROTOCOL = 'http'
-export const TEIGARAGE_LOCATION = 'localhost:8080'
+// export const TEIGARAGE_PROTOCOL = 'http'
+// export const TEIGARAGE_LOCATION = 'localhost:8080'
 
 export const DATASOURCES_PROTOCOL = 'https'
 export const DATASOURCES_LOCATION = 'tei-c.org/Vault/P5/current/xml/tei/odd'
