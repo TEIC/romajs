6573009a64798fa7004d1adbd256ff587d63900c
Raffaele Viglianti <raffaeleviglianti@gmail.com>
2025-10-07 11:21:20 -0400

    Partial fix for #186. It will be completed once this PR is merged: https://github.com/TEIC/Stylesheets/pull/781

--- src/containers/BlocklyContainer.js
+++ src/containers/BlocklyContainer.js
@@ -4,7 +4,7 @@ import { updateContentModel } from '../actions/elements'
 import { clone } from '../utils/clone'
 
 const flattenContentModel = (cnt, flattened = [], depth = 1) => {
-  cnt.map(c => {
+  cnt.forEach(c => {
     let copy = clone(c)
     switch (c.type) {
       case 'sequence':
@@ -13,10 +13,10 @@ const flattenContentModel = (cnt, flattened = [], depth = 1) => {
         const content = copy.content.slice(0)
         copy.content = true
         flattened.push(copy)
+        // Process children at depth + 1
         flattenContentModel(content, flattened, depth + 1)
         break
       default:
-        copy = clone(c)
         copy.depth = depth
         flattened.push(copy)
     }
