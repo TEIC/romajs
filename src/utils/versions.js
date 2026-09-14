b45397a8253aa33cc1a124c3ab58a0aa9418ba06
Raffaele Viglianti <raffaeleviglianti@gmail.com>
2026-09-14 15:11:50 -0400

    added latest versions

--- src/utils/versions.js
+++ src/utils/versions.js
@@ -1,15 +1,15 @@
 // This is meant to be a temporary solution until metadata about ODD_based schemas can be shared more reliably.
 
 export const TEI_VERSIONS = [
+  '4.11.0', '4.12.0',
   '4.10.2', '4.10.1',
-  '4.10.0',
-  '4.9.0', '4.8.1',
-  '4.8.0', '4.7.0',
-  '4.6.0', '4.5.0',
-  '4.4.0', '4.3.0',
-  '4.2.2', '4.2.1',
-  '4.2.0', '4.1.0',
-  '4.0.0'
+  '4.10.0', '4.9.0',
+  '4.8.1', '4.8.0',
+  '4.7.0', '4.6.0',
+  '4.5.0', '4.4.0',
+  '4.3.0', '4.2.2',
+  '4.2.1', '4.2.0',
+  '4.1.0', '4.0.0'
 ]
 
 export const TEI_CURRENT = 'current'
